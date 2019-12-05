00cc05b241a12654ca75ad31de1a423ae63e0a91
JaoK <JJaoKK@gmail.com>
2019-12-05 09:12:23 +0100

    Create oop.java

---
+++ OOP/oop.java
@@ -0,0 +1,19 @@
+public class oop {
+
+    public static void main(String[] args) {
+
+        Person joakim = new Person("Joakim",24,75,185);
+        joakim.printInfo();
+
+        Person oskar = new Person("Oskar",17,73,180);
+        oskar.printInfo();
+
+        Person mojje = new Person("Mojje",18,74,190);
+        mojje.printInfo();
+
+        Person max = new Person("Max",17,85,197);
+        max.printInfo();
+
+    }
+
+}
